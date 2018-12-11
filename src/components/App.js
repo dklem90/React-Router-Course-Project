cd1a7eb98e019ab60a2809b8ad4c6d96c2af0aa6
dklem <dklem90@gmail.com>
2018-12-11 16:19:00 -0600

    Added article component to project

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
+import Navbar from './Navbar';
+import TeamPage from './TeamPage';
+import Articles from './Articles';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          
+          <Navbar />
+          
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/players' component={Players} />
+            <Route path='/teams' component={Teams} />
+            <Route exact path='/:teamId' component={TeamPage} />
+            <Route path='/:teamId/articles' component={Articles} />
+            <Route render={() => <h1 className='text-center'>Four oh Four.</h1>} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
